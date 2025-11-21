48e0dbaaff3926411d060530f11976ec57f8be23
prakhar0085 <prakhartiwari942@gmail.com>
2025-11-21 10:26:20 +0530

    first commit

---
+++ LMS/backend/configs/token.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken"
+export const genToken= async(userId)=>{
+try {
+   let token= jwt.sign({userId} , process.env.JWT_SECRET , {expiresIn:"7d"})
+   return token
+} catch (error) {
+    console.log("token error")
+}
+
+
+}
+
+// const genToken = async (userId) => {
+//     try{
+//         const token = await jwt.sign({userId} , process.env.JWT_SECRET , {expiresIn:"7d"})
+//         console.log(token)
+
+//     } catch (error) {
+//         console.log(error)
+
+//     }
+// }
+// export default genToken
